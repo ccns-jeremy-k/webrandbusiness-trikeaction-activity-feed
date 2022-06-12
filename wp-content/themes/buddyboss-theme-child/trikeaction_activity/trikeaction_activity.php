6e02922a9003fa8e0ff4ac219f0f8f7890f4eb18
Jeremy Kavuncuoglu <jeremy.k@camelcasenospace.com>
2022-06-12 15:01:58 -0400

    Move create_new_activity

--- wp-content/themes/buddyboss-theme-child/trikeaction_activity/trikeaction_activity.php
+++ wp-content/themes/buddyboss-theme-child/trikeaction_activity/trikeaction_activity.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(__FILE__)."/feed.php";
+require dirname(__FILE__) . "/feed.php";
 
 /**
  * Trikeaction Activities
@@ -12,7 +12,7 @@ require dirname(__FILE__)."/feed.php";
 
 function taa_get_activities($attrs): string
 {
-    if (! defined( 'REST_REQUEST' )) {
+    if (! defined('REST_REQUEST')) {
         new Feed($attrs);
     }
 
@@ -21,32 +21,29 @@ function taa_get_activities($attrs): string
 
 function taa_init($attrs): string
 {
-    taa_create_new_video_activity();
+    add_action('comment_post', 'create_new_activity', 10, 3);
     taa_add_activities_styles_and_scripts();
     return taa_get_activities($attrs);
 }
 
-function taa_create_new_video_activity(): void
+function create_new_activity($comment_id, $approved, $commend_data): void
 {
-    add_action('comment_post', 'create_new_activity', 10, 3);
-    function create_new_activity($comment_id, $approved, $commend_data): void
-    {
-        $post = get_post($commend_data['comment_post_ID']);
-        if ($post->post_type == 'aiovg_videos') {
-            global $wpdb;
-            $user = get_user_by('ID', bp_loggedin_user_id());
-            $action = "<a href=\"http://localhost:8000/members/{$user->user_nicename}/\">{$user->display_name}</a> commented on Video <a href=\"{$post->guid}\">`{$post->post_title}</a>`";
-            $content = $commend_data['comment_content'];
-            $component = 'activity';
-            $type = "activity_comment";
-            $primary_link = $post->guid;
-            $user_id = bp_loggedin_user_id();
-            $item_id =  $post->ID;
-            $recorded_time = bp_core_current_time();
-            $privacy = 'public';
-            $wpdb->query(
-                $wpdb->prepare(
-                    "INSERT INTO {$wpdb->prefix}bp_activity (
+    $post = get_post($commend_data['comment_post_ID']);
+    if ($post->post_type == 'aiovg_videos') {
+        global $wpdb;
+        $user = get_user_by('ID', bp_loggedin_user_id());
+        $action = "<a href=\"http://localhost:8000/members/{$user->user_nicename}/\">{$user->display_name}</a> commented on Video <a href=\"{$post->guid}\">`{$post->post_title}</a>`";
+        $content = $commend_data['comment_content'];
+        $component = 'activity';
+        $type = "activity_comment";
+        $primary_link = $post->guid;
+        $user_id = bp_loggedin_user_id();
+        $item_id = $post->ID;
+        $recorded_time = bp_core_current_time();
+        $privacy = 'public';
+        $wpdb->query(
+            $wpdb->prepare(
+                "INSERT INTO {$wpdb->prefix}bp_activity (
                               user_id, 
                               component, 
                               type, 
@@ -59,27 +56,26 @@ function taa_create_new_video_activity(): void
                               is_spam, 
                               privacy)
                        values (%d, %s, %s, %s, %s, %s, %d, %s, %s, %s, %s)",
-                    $user_id,
-                    $component,
-                    $type,
-                    $action,
-                    $content,
-                    $primary_link,
-                    $item_id,
-                    $recorded_time,
-                    false,
-                    false,
-                    $privacy
-                )
-            );
-        }
+                $user_id,
+                $component,
+                $type,
+                $action,
+                $content,
+                $primary_link,
+                $item_id,
+                $recorded_time,
+                false,
+                false,
+                $privacy
+            )
+        );
     }
 }
 
 function taa_add_activities_styles_and_scripts(): void
 {
-    wp_enqueue_script( 'taa_activities_scripts', dirname(__FILE__).'/taa_activities.js', ['jquery'], null, $footer = true );
-    wp_enqueue_style( 'taa_activities_styles', dirname(__FILE__).'/taa_activities.css', [], null);
+    wp_enqueue_script('taa_activities_scripts', dirname(__FILE__) . '/taa_activities.js', ['jquery'], null, $footer = true);
+    wp_enqueue_style('taa_activities_styles', dirname(__FILE__) . '/taa_activities.css', [], null);
 }
 
 add_shortcode('trikeaction_activities', 'taa_init');
